2b35be4d97f9f44d5c1a341752357ac9a41b35e0
crchende <57460107+crchende@users.noreply.github.com>
2023-04-09 21:26:54 +0300

    Update dockerstart.sh

--- dockerstart.sh
+++ dockerstart.sh
@@ -1,9 +1,12 @@
 #!/bin/sh
 echo "changing directory to app:"
 cd ./app
+echo `ls -l`
 echo "Activare venv:"
 #source ../.vanv/bin/activate
 . ../.venv/bin/activate
+echo `ls -l`
+echo `which flask`
 echo "Configurare variabila mediu FLASK_APP"
 export FLASK_APP=sysinfo
 echo "Start server:"
