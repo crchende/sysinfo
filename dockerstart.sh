ffc7d6915614203c03605b216c57746476fe119d
Ciprian Chende-Roman <cip_chende@yahoo.com>
2023-08-06 09:15:36 +0300

    Restructurare cod - v02

--- dockerstart.sh
+++ dockerstart.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 echo "Activare venv:"
-#source ../.vanv/bin/activate
-. ../.venv/bin/activate
+#source .vanv/bin/activate
+. .venv/bin/activate
 echo "Configurare variabila mediu FLASK_APP"
 export FLASK_APP=sysinfo
 echo "Start server:"
-exec flask run -h 0.0.0.0 -p 5020 --reload
+exec flask run -h 0.0.0.0 -p 5011 --reload
